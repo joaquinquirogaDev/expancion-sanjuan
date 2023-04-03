b26b6c179d08e08ae3d3088178fb1d95e47c4ec8
Fede Rocha <federochacueli@gmail.com>
2023-04-03 20:12:43 -0300

    Last modal

--- src/Components/Modals/Turn/Turn.jsx
+++ src/Components/Modals/Turn/Turn.jsx
@@ -8,11 +8,12 @@ import style from '../Modal.module.css'
 import { IoClose } from "react-icons/io5";
 
 // Images
-import User from '../../../assets/images/user.svg'
-import LogoVertical from '../../../assets/images/logos/LogoVertical.svg'
+import LogoCompleto from '../../../assets/images/logos/logoCompleto.svg'
 
 const customStyles = {
      content: {
+          width: '100%',
+          maxWidth: '600px',
           maxHeight: '600px',
           top: '50%',
           left: '50%',
@@ -49,16 +50,34 @@ export default function Turn() {
                     </div>
                     <div className={style.modal__body}>
                          <div className={style.modal__turn}>
-                              <div>
+                              <div className={style.modal__turn__title}>
                                    <h2>Tu turno será solicitado con los siguientes datos:</h2>
                               </div>
                               <p>Nombre y Apellido del solicitante del préstamo:</p>
-                              <div>
+                              <div className={style.modal__turn__date}>
                                    <p>Fecha y hora del turno solicitado </p>
                                    <div>
                                         <h3>Fecha:</h3>
                                         <h3>Hora:</h3>
                                    </div>
+                                   <button className='btn__bordo'>EDITAR</button>
+                              </div>
+                              <div className={style.modal__turn__data}>
+                                   <p>Tamaño de empresa: </p>
+                                   <h3>Micro</h3>
+                                   <p>Tipo de empresa:</p>
+                                   <h3>Explotación Unipersonal</h3>
+                                   <p>Monto:</p>
+                                   <h3>$10.000</h3>
+                                   <p>Necesidad financiera </p>
+                                   <h3>Inversiones</h3>
+                                   <p>De que manera te enteraste de expansión San Juan:</p>
+                                   <h3>Redes sociales</h3>
+                                   <button className='btn__bordo'>EDITAR</button>
+                              </div>
+                              <div className={style.modal__turn__btn}>
+                                   <button className='btn__bordo'>CONFIRMAR</button>
+                                   <img src={LogoCompleto} alt="Ministerio de produccion" />
                               </div>
                          </div>
                     </div>
