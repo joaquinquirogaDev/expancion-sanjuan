48a18129c2bd5c670c66d7c474d59811e2efa233
Fede Rocha <federochacueli@gmail.com>
2023-05-03 00:50:16 -0300

    Fixes

--- src/Components/Sections/SectionOne.jsx
+++ src/Components/Sections/SectionOne.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 // Styles
 import style from './Sections.module.css'
 
@@ -8,38 +10,44 @@ import Mediana from '../../assets/images/type/mediana.svg'
 import Grande from '../../assets/images/type/grande.svg'
 
 export default function SectionOne() {
+
+    const [amarillo, setAmarillo] = useState(false)
+    const [rojo, setRojo] = useState(false)
+    const [naranja, setNaranja] = useState(false)
+    const [bordo, setBordo] = useState(false)
+
     return (
         <div className={style.section}>
             <h2>Selecciona tamaño de la empresa según SePyMe</h2>
             <br />
             <div className={style.sectionOne__cards}>
-                <div className={style.sectionOne__card}>
-                    <div className={style.sectionOne__card__image}>
-                        <img src={Micro} alt="" />
+                <div className={style.sectionOne__card} onClick={() => {setNaranja(!naranja); setAmarillo(false); setRojo(false); setBordo(false)}}>
+                    <div className={naranja ? style.sectionOne__card__naranja : style.sectionOne__card__image}>
+                        <img className={style.sectionOne__card__img} src={Micro} alt="" />
                     </div>
                     <div className={style.sectionOne__card__title}>
                         <h3>Micro</h3>
                     </div>
                 </div>
-                <div className={style.sectionOne__card}>
-                    <div className={style.sectionOne__card__image}>
-                        <img src={Pequeña} alt="" />
+                <div className={style.sectionOne__card} onClick={() => {setNaranja(false); setAmarillo(!amarillo); setRojo(false); setBordo(false)}}>
+                    <div className={amarillo ? style.sectionOne__card__amarillo : style.sectionOne__card__image}>
+                        <img className={style.sectionOne__card__img} src={Pequeña} alt="" />
                     </div>
                     <div className={style.sectionOne__card__title}>
                         <h3>Pequeña</h3>
                     </div>
                 </div>
-                <div className={style.sectionOne__card}>
-                    <div className={style.sectionOne__card__image}>
-                        <img src={Mediana} alt="" />
+                <div className={style.sectionOne__card} onClick={() => {setNaranja(false); setAmarillo(false); setRojo(!rojo); setBordo(false)}}>
+                    <div className={rojo ? style.sectionOne__card__rojo : style.sectionOne__card__image}>
+                        <img className={style.sectionOne__card__img} src={Mediana} alt="" />
                     </div>
                     <div className={style.sectionOne__card__title}>
                         <h3>Mediana</h3>
                     </div>
                 </div>
-                <div className={style.sectionOne__card}>
-                    <div className={style.sectionOne__card__image}>
-                        <img src={Grande} alt="" />
+                <div className={style.sectionOne__card} onClick={() => {setNaranja(false); setAmarillo(false); setRojo(false); setBordo(!bordo)}}>
+                    <div className={bordo ? style.sectionOne__card__bordo : style.sectionOne__card__image}>
+                        <img className={style.sectionOne__card__img} src={Grande} alt="" />
                     </div>
                     <div className={style.sectionOne__card__title}>
                         <h3>Grande</h3>
--- src/Components/Slice/Slice.jsx
+++ src/Components/Slice/Slice.jsx
@@ -1,7 +1,17 @@
 import style from '../Slice/Slice.module.css'
 
 export default function Slice({setStep, step, array}){
-    return array?.map(item => (
-        <div className={ item?.item ? style.Slices : style.SlicesWhite }>{item?.id}</div>
-    ))
+    
+    return (
+        <>
+            <div className={step <= 0 ? style.SlicesWhite : style.Slices}></div>
+            <div className={step <= 1 ? style.SlicesWhite : style.Slices}></div>
+            <div className={step <= 2 ? style.SlicesWhite : style.Slices}></div>
+            <div className={step <= 3 ? style.SlicesWhite : style.Slices}></div>
+            <div className={step <= 4 ? style.SlicesWhite : style.Slices}></div>
+            <div className={step <= 5 ? style.SlicesWhite : style.Slices}></div>
+            <div className={step <= 6 ? style.SlicesWhite : style.Slices}></div>
+            <div className={step <= 7 ? style.SlicesWhite : style.Slices}></div>
+        </>
+    )
 }
